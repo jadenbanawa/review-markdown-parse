910855b02410a00a1dc9565a17a2f3a7d3143960
jadenbanawa <jkbanawa@ucsd.edu>
2022-06-02 14:06:42 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,51 +8,30 @@ import org.junit.*;
 public class MarkdownParseTest {
 
     @Test
-    public void testFile1() throws Exception {
-        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("test-file.md")));
-        String[] expected = {"https://something.com", "some-thing.html"};
+    public void snippet1test1() throws Exception{
+    String[] expected = {"`google.com"};
 
-        assertArrayEquals(expected, links.toArray());
+    ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("/Users/Jaden/Documents/GitHub/markdown-parser/snippet1.md")));
+    
+    assertArrayEquals(expected, links.toArray());
     }
 
     @Test
-    public void testFile5() throws Exception {
-        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("test-file5.md")));
-        String[] expected = {};
+    public void snippet1test2() throws Exception{
+    String[] expected = {"a.com","a.com(())","example.com"};
 
-        assertArrayEquals(expected, links.toArray());
-    }
-
-    @Test
-    public void testPaddingMultiline() throws Exception {
-        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("test-breaking-padding-multiline.md")));
-        String[] expected = {"https://something.co.uk"};
-
-        assertArrayEquals(expected, links.toArray());
-    }
-
-    @Test
-    public void testBackticks() throws Exception {
-        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("test-backticks.md")));
-        String[] expected = {"some-thing.html"};
-
-        assertArrayEquals(expected, links.toArray());
-    }
-
-    @Test
-    public void testUrlEscape() throws Exception {
-        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("test-url-escape.md")));
-        String[] expected = {};
-
-        assertArrayEquals(expected, links.toArray());
+    ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("/Users/Jaden/Documents/GitHub/markdown-parser/snippet2.md")));
+    
+    assertArrayEquals(expected, links.toArray());
     }
 
 
     @Test
-    public void testImage() throws Exception {
-        ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("test-breaking-image.md")));
-        String[] expected = {"https://something.com", "https://something.edu"};
+    public void snippet1test3() throws Exception{
+    String[] expected = {"https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"};
 
-        assertArrayEquals(expected, links.toArray());
+    ArrayList<String> links = MarkdownParse.getLinks(Files.readString(Path.of("/Users/Jaden/Documents/GitHub/markdown-parser/snippet3.md")));
+    
+    assertArrayEquals(expected, links.toArray());
     }
 }
